928be2cfa97c92c3d6636c6d968c05f60d76c509
Celia <celia2795@gmail.com>
2023-06-01 17:12:32 +0200

    principio de segregacion

---
+++ src/solid-principles/04-segregation.ts
@@ -0,0 +1,47 @@
+
+// configuramos las interfaces de forma que no obligamos
+// a nuestras clases a implementar métodos que no van a usar
+// esto también las hace más tolerantes al cambio
+interface Bird {
+    eat(): void;
+}
+
+interface FlyingBird {
+    fly(): void;
+}
+
+interface RunningBird {
+    run(): void;
+}
+
+interface SwimmingBird {
+    swim(): void;
+}
+
+class Tucan implements Bird, FlyingBird {
+
+    public fly() { }
+    public eat() { }
+
+}
+
+
+class Hummingbird implements Bird, FlyingBird {
+
+    public fly() { }
+    public eat() { }
+
+}
+
+
+class Ostrich implements Bird, RunningBird {
+
+    public eat() { }
+    public run() { }
+}
+
+class Penguin implements Bird, SwimmingBird {
+
+    public eat() { }
+    public swim() { }
+}
